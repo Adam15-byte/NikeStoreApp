24a16ac4aae9a18fa5ff66b396cc7b78fb43fce5
Adam <AdamJankowski5@gmail.com>
2022-04-14 09:02:36 +0200

    first commit. Prepared data, navigation and images.

---
+++ Navigation.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import BrowseScreen from "./src/screens/BrowseScreen";
+import ShoppingCart from "./src/screens/ShoppingCart";
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="BrowseScreen" component={BrowseScreen} />
+        <Stack.Screen name="ShoppingCart" component={ShoppingCart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
+
+const styles = StyleSheet.create({});
